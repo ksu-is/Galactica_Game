9fd23ec39febb3dad7cb4d1b9e169e232ab93b22
Aidan Adams <aadam262@students.kennesaw.edu>
2025-11-21 12:27:35 -0500

    Initialize pygame and set up game variables

---
+++ ProjectCode.py
@@ -0,0 +1,34 @@
+import pygame
+import random
+
+
+
+pygame.init()
+
+
+
+screen_w = 800
+
+screen_h = 600
+
+fps = 60
+
+play_s = 5
+
+bullet_s = 7
+
+enemy_s = 2
+
+enemy_num = 10
+
+w_score = 1000
+
+
+
+wh = (255, 255, 255)
+
+red = (255, 0, 0)
+
+bl = (0, 0, 0)
+
+gr = (0, 255, 0)
